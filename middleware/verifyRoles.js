40ad1291e30292ab980c25bb926b66dc6c558944
Or mizrahi <108184274+OrMizrahi12@users.noreply.github.com>
2022-10-27 10:01:59 +0300

    Update verifyRoles.js

--- middleware/verifyRoles.js
+++ middleware/verifyRoles.js
@@ -1,18 +1,12 @@
-// #22 the function verifyRoles get the roles_list from workers.js
 const verifyRoles = (...allowedRoles) => {
     return (req, res, next) => {
-        // #23 check if there is roles
-        if (!req?.roles) return res.sendStatus(401);
+        if (!req?.roles)
+            return res.sendStatus(401);
         const rolesArray = [...allowedRoles];
-        // #24 arr on the roles !--OF THE USER--!
-        // map - one per one item ,this code is find if the role is inclode in the rolesArray --> 
-        // find - if the value is exsist 
         const result = req.roles.map(role => rolesArray.includes(role)).find(val => val === true);
-        if (!result) return res.sendStatus(401);
+        if (!result) 
+            return res.sendStatus(401);
         next();
     }
 }
-
 module.exports = verifyRoles
-
-// #25 done the process.
