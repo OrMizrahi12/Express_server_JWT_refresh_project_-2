e665a26d422c1339a3c8bb7159fc4aad52c133d8
Or mizrahi <108184274+OrMizrahi12@users.noreply.github.com>
2022-10-27 10:00:52 +0300

    Update credentials.js

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -1,12 +1,8 @@
-// this is for CORS policy
-// #20
+
 const allowedOrigins = require('../config/allowedOrigins');
 
 const credentials = (req, res, next) => {
-    // here we fet the origin
     const origin = req.headers.origin;
-
-    // if origin is exsist, we send response that will resolve the CORS!!
     if (allowedOrigins.includes(origin)) {
         res.header('Access-Control-Allow-Credentials', true);
     }
@@ -14,5 +10,3 @@ const credentials = (req, res, next) => {
 }
 
 module.exports = credentials
-
-// for #21 go to sever.js --> 
